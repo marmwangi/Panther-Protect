a6a7c3078e7e40a40e1528757812015e6738be8d
M322-tech-debug <mwangimartha82@gmail.com>
2023-11-06 18:43:28 -0500

    edited app.py

--- back-end/app.py
+++ back-end/app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template 
+from . import views
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
+app = Flask(__name__)
+app.register_blueprint(view, url_prefix='/')
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/map')
+def map():
+    return render_template('Slay')
+
 if __name__ == '__main__':
     app.run(debug=True)
-    

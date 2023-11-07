04376a56e2e90c617018a82d18635f571e15e26b
M322-tech-debug <mwangimartha82@gmail.com>
2023-11-06 19:25:36 -0500

    made it so views/routes are in a seperate folder

--- back-end/app.py
+++ back-end/app.py
@@ -1,16 +1,8 @@
-from flask import Flask, render_template 
-from . import views
+from flask import Flask
+from views import view
 
 app = Flask(__name__)
 app.register_blueprint(view, url_prefix='/')
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/map')
-def map():
-    return render_template('Slay')
 
 if __name__ == '__main__':
     app.run(debug=True)
